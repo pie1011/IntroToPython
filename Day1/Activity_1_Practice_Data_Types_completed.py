a42986d9b7d6ee75199e232d86e8f27aac6acead
Katie <pie10101011@gmail.com>
2022-10-15 10:47:25 -0700

    Completed activity 1

---
+++ Day1/Activity_1_Practice_Data_Types_completed.py
@@ -0,0 +1,105 @@
+## Section 1 - variables and functions: 
+
+# Question 1: Create two variables. One should be a string data type, and the other should be of type int.
+
+my_string = "Hello world"
+my_int = 42
+
+# Use a single print statement to print both variables:
+
+print(my_string, my_int)
+
+# Other formatting:
+
+print(f"{my_string} {my_int}" )
+
+# Question 2: Create a Python function that prints a greeting with a name as the parameter.
+
+def greeting(name):
+    print("hello,", name)
+
+# Invoke the function with a name argument of your choosing:
+
+greeting("Katie")
+
+## Section 2 - lists:
+
+# Question 3: Create a list of your favorite movies with  at least 4 elements:
+
+movies = ['Star Wars', 'Lion King', 'Endgame', 'Top Gun: Maverick']
+
+# Question 4: Use a print statement to print the list item at the index of 1:
+
+print(movies[1])
+
+# Question 5: Append a movie to the end of your list:
+
+movies.append('Pet Semetary')
+
+# Use a print statement to print this ammended list:
+
+print(movies)
+
+## Section 3 - dictionaries:
+
+# Question 6: Create a dictionary named 'cellphone' with 2 key:value pairs that are the properties of your cellphone.
+#The keys should be: "color" and "number".
+#Fill out the values on your own:
+
+cellphone = {
+    "color" : "teal",
+    "number" : 42
+}
+
+# Question 7: Access a value from inside the dictionary (Try to print the value of the 'color' property).
+
+print(cellphone["color"])
+
+## Section 4 - strings:
+
+# Question 8: Create a variable and store a string with multiple words in it:
+
+string_to_change = "panic at the disco"
+
+# Question 9: Utilize the method that capitalizes the first letter of each word in your string - store this new string in a new variable:
+
+band = string_to_change.title()
+
+# Use a print statement to print the new string:
+
+print(band)
+
+
+#Bonus:
+
+# Question 10: Uncomment and look into this nested dictionary - try to relate your knowledge of objects and arrays in JavaScript.
+
+students_in_order = {
+  1: {'name': 'Esteban', 'age': '27', 'eye color': 'blue'},
+  2: {'name': 'Jackson', 'age': '22', 'eye color': 'brown'},
+  3: {'name': 'Zayn', 'age': '26', 'eye color': 'green'}
+}
+
+# Question 10 A: Write a print function that outputs the second student in the dictionary
+
+def second():
+    student = students_in_order[2]
+    print(student)
+
+second()
+
+# Question 10 B: Write a print statement that outputs the name "Zayn" using the dictionary
+
+def zzz():
+    student = students_in_order[3]['name']
+    print(student)
+
+zzz()
+
+# Question 10 C: Write a print statment that outputs the age of Esteban from the dictionary
+
+def esteban_age():
+    student = students_in_order[1]["age"]
+    print(student)
+
+esteban_age()
